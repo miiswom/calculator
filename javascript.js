b4cb5781974dd5a2a4ba2a662b864aa473e3eaca
Farmata Kane <kane.farmata@gmail.com>
2023-08-07 08:10:34 +0100

    Create a basic HTMl calculator version without proper digits

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// basic math operations
 function add(a, b) {
     return a + b;
 };
@@ -13,6 +14,30 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+// variables for each part of calculator operations
+let firstNum = 0;
+let operator = ['+', '-', '*', '/'];
+let secondNum = 0;
+
+function operate(numA, op, numB) {
+    let result = add(numA, numB)
+    return result;
+    // return "The operation is " + numA + " " + op + " " + numB + " .";
+}
+
+// creates digit buttons
+
+const digits = document.getElementById('digits');
+
+(function createDigits() {
+    for (let i = 0; i < 16; i++) {
+        let digitButtons = document.createElement('button');
+        digits.appendChild(digitButtons);
+        digitButtons.textContent = [i];
+        digitButtons.classList.add('digit-buttons')
+    };
+})()
 
 
 
