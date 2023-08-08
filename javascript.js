1cbf68b3952bae83257578aa08e2b240df2b49d5
Farmata Kane <kane.farmata@gmail.com>
2023-08-08 09:16:35 +0100

    Classify operators buttons and digits buttons

--- javascript.js
+++ javascript.js
@@ -19,34 +19,45 @@ function divide(a, b) {
 let firstNum = 0;
 let operator = ['+', '-', '*', '/'];
 let secondNum = 0;
+let result = '0'
 
 function operate(numA, op, numB) {
-    let result = add(numA, numB)
-    return result;
-    // return "The operation is " + numA + " " + op + " " + numB + " .";
+    op = operatorClick.textContent;
+    numA = firstDigitClick.textContent;
+    numB = secondDigitClick.textContent;
+        if (op === operator[0]) {
+        result = add(numA, numB);
+    } else if (op === operator[1]) {
+        result = subtract(numA, numB);
+    } else if (op === operator[2]) {
+        result = multiply(numA, numB);
+    } else if (op === operator[3]) {
+        result = divide(numA, numB);
+    } console.log(result);
 }
 
 // creates digit buttons
-
 const digits = document.getElementById('nine-digits');
-
-// (function createDigits() {
-//     for (let i = 0; i < 16; i++) {
-//         let digitButtons = document.createElement('button');
-//         digits.appendChild(digitButtons);
-//         digitButtons.textContent = [i];
-//         digitButtons.classList.add('digit-buttons')
-//     };
-// })()
 
 (function createDigitsToNine() {
     for (let i = 1; i < 10; i++) {
         let digitButtons = document.createElement('button');
         digits.appendChild(digitButtons);
         digitButtons.textContent = [i];
-        digitButtons.classList.add('digit-buttons')
+        digitButtons.classList.add('digit-buttons', 'numbers')
     };
 })()
+
+const digitClick = document.getElementsByClassName('numbers')
+
+for (let i=0; i < digitClick.length; i++) {
+    digitClick[i].addEventListener('click', test);
+}
+
+function test () {
+    console.log(digitClick.textContent)
+}
+// create operator buttons
 
 const rightContainerSide = document.getElementById('operators');
 
@@ -55,17 +66,19 @@ const rightContainerSide = document.getElementById('operators');
         let operatorButton = document.createElement('button');
         rightContainerSide.appendChild(operatorButton);
         operatorButton.textContent = operator[i];
-        operatorButton.classList.add('digit-buttons', 'operator')
+        operatorButton.classList.add('digit-buttons', 'operators')
     };
-})()
+})() 
 
-// const bottomContainer = document.getElementById('bottom-buttons');
 
-// (function createBottomButtons() {
+// let operatorButton = function () {
 //     for (let i = 0; i < 4; i++) {
-//         let bottomButton = document.createElement('button');
-//         bottomContainer.appendChild(bottomButton);
-//         // digitButtons.textContent =
-//         bottomButton.classList.add('digit-buttons')
+//         document.createElement('button');
+//         rightContainerSide.appendChild(operatorButton);
+//         operatorButton.textContent = operator[i];
+//         operatorButton.classList.add('digit-buttons', 'operator')
 //     };
-// })()
+// }
+
+let operatorClick = document.getElementsByClassName('operators');
+operatorClick[1].addEventListener('click', operate);
